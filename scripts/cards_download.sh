94ea73752407a00ceef61f13a3f92a1019fa3af5
ys87 <ys87yy19@gmail.com>
2026-01-18 17:27:03 +0800

    可以编译的版本

--- scripts/cards_download.sh
+++ scripts/cards_download.sh
@@ -5,5 +5,5 @@ HSJSON="https://api.hearthstonejson.com/v1"
 
 for lang in deDE enUS esES esMX frFR itIT jaJP koKR plPL ptBR ruRU thTH zhCN zhTW; do
 	echo "Downloading cards.$lang.json"
-	wget "$HSJSON/latest/$lang/cards.json" -O "$BASEDIR/../HSTracker/Resources/Cards/cardsDB.$lang.json"
+	curl -x 127.0.0.1:7890 -L "$HSJSON/latest/$lang/cards.json" -o "$BASEDIR/../HSTracker/Resources/Cards/cardsDB.$lang.json"
 done
